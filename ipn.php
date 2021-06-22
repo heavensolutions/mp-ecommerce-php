53073132f58291762554307c0f2fd85e3248bf66
heavencommerce <lucas@heavenonline.com.ar>
2021-06-22 12:24:20 -0300

    minor fixes

--- ipn.php
+++ ipn.php
@@ -4,8 +4,11 @@ http_response_code(200);
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-if($payment->status == "approved"){
-    echo json_encode($payment);
+if ($payment->status == "approved") {
+    
+    $json = json_encode($payment);
+    $arch = fopen("json.txt", "a+");
+    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $json\n");
+    fclose($arch);
+    
 }
-
-?>
