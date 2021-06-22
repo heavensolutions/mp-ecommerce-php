2972ba86dd8c917b2a569f286ee6e5b3c05b32fe
heavencommerce <lucas@heavenonline.com.ar>
2021-06-22 17:49:58 -0300

    fix ipn

--- ipn.php
+++ ipn.php
@@ -1,44 +1,49 @@
 <?php
 require_once("vendor/autoload.php");
 http_response_code(200);
-
-// Takes raw data from the request
-$json = file_get_contents('php://input');
-
-// Converts it into a PHP object
-$data = json_decode($json);
-
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-switch ($_POST["type"]) {
+$arch = fopen("json.txt", "a+");
+
+
+$input = file_get_contents('php://input');
+fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] POST (file_get_contents):  $input\n");
+
+
+$data = json_encode($input);
+fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] POST (json encode):  $data\n");
+
+
+$data = json_decode($input);
+fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] POST (json decode):  $data\n");
+
+
+$data = json_encode(parse_str($_SERVER['QUERY_STRING']));
+fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] QUERY STRING:  $data\n");
+
+switch ($_GET["type"]) {
     case "payment":
-        $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
+        $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
         break;
     case "plan":
-        $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
+        $plan = MercadoPago\Plan::find_by_id($_GET["id"]);
         break;
     case "subscription":
-        $plan = MercadoPago\Subscription::find_by_id($_POST["id"]);
+        $plan = MercadoPago\Subscription::find_by_id($_GET["id"]);
         break;
     case "invoice":
-        $plan = MercadoPago\Invoice::find_by_id($_POST["id"]);
+        $plan = MercadoPago\Invoice::find_by_id($_GET["id"]);
         break;
 }
-
-$json = json_encode($_POST);
-$arch = fopen("json.txt", "a+");
-fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $data\n");
-fclose($arch);
 
 if ($payment->status == "approved") {
 
-    $json = json_encode($payment);
-    $arch = fopen("json.txt", "a+");
-    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $json\n");
-    fclose($arch);
+    $payment = json_encode($payment);
+    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] payment: $payment\n");
+    
 } else {
 
-    $arch = fopen("json.txt", "a+");
-    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] no guardo nada :)  \n");
-    fclose($arch);
+    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] payment status:  $payment->status  \n");
 }
+
+fclose($arch);
