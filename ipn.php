269721357c4e36851e1e9b331e21756676c228b0
heavencommerce <lucas@heavenonline.com.ar>
2021-06-22 15:28:42 -0300

    fix ipn

--- ipn.php
+++ ipn.php
@@ -4,7 +4,7 @@ http_response_code(200);
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-switch($_POST["type"]) {
+switch ($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
         break;
@@ -19,22 +19,20 @@ switch($_POST["type"]) {
         break;
 }
 
+$json = json_encode($_POST);
+$arch = fopen("json.txt", "a+");
+fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $json\n");
+fclose($arch);
+
 if ($payment->status == "approved") {
-    
+
     $json = json_encode($payment);
     $arch = fopen("json.txt", "a+");
     fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $json\n");
     fclose($arch);
+} else {
 
-    $json = json_encode($_POST);
     $arch = fopen("json.txt", "a+");
-    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] $json\n");
+    fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] no guardo nada :)  \n");
     fclose($arch);
-    
-}else{
-    
-        $arch = fopen("json.txt", "a+");
-        fwrite($arch, "[" . date("Y-m-d H:i:s.u") . "] no guardo nada :)  \n");
-        fclose($arch);
-
 }
